edf1edbda88286f4bf7e4c676fb9c6acf98b9a51
Ogheneovie Ralph Otutu <phexcom@phexcom.me>
2018-04-02 23:37:11 +0400

    Update Email.php
    Remove Email Details

--- application/config/email.php
+++ application/config/email.php
@@ -2,10 +2,10 @@
 
 // Email settings
 
-$config['protocol'] = 'smtp';
-$config['smtp_host'] = 'smtp.gmail.com';
-$config['smtp_user'] = 'carphex@gmail.com';
-$config['smtp_pass'] = 'carphexwhydead6728263';
+$config['protocol'] = '';
+$config['smtp_host'] = '';
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
 $config['smtp_crypto'] = 'ssl';
 $config['mailtype'] = 'html';
 $config['newline'] = "\r\n";
